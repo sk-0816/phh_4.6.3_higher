7dcb8e583a8f8a946fdb7bb900a8566d3542de8b
sk-0816 <kouhei17n1100371@nnn.ed.js>
2017-07-10 16:00:47 +0900

    高階関数の活用

--- higher.js
+++ higher.js
@@ -1,13 +1,20 @@
+//高階関数
 function arrayWalk(data,f){
     for(let key in data){
         f(data[key],key);
     }
 }
 
+//コールバック関数
 function showElement(value,key){
     console.log(key+':'+value);
 }
+//コールバック関数
+function showDecoratedElement(value,key){
+    console.log('キー(' + key + '):値(' + value + ')');
+
+}
 
 let ary = [1,2,4,8,16];
-// arrayWalk(ary,showElement);
-ary.forEarch(showElement);
+arrayWalk(ary,showElement);
+//ary.forEarch(showElement);
